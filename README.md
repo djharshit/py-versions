0cd99201453da75235ab4ca2b1e876ec14eb21d7
Harshit M <harshitmeh111@gmail.com>
2022-09-04 10:44:58 +0530

    Added the code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # py-versions
-Top 10 python versions
+
+Program to print top 10 python versions along with the date when they are released.
+
+### Installation
+```
+$ git clone https://github.com/djharshit/py-versions
+```
+
+### Run
+```
+$ cd py-versions
+$ python versions.py
+```
+### Libraries
+* requests
+* BeautifulSoup
---
+++ version.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+try:
+    res = requests.get('https://www.python.org/downloads/')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    items = soup.find_all(name='ol', class_='list-row-container menu')[1]
+    version = items.find_all(name='span', class_='release-number')
+    date = items.find_all(name='span', class_='release-date')
+
+    for i in range(10):
+        print(version[i].text, date[i].text)
+
+except:
+    print('Not connected')
+
+finally:
+    input()
