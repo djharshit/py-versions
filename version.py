fef3c5f9107ae4a11c5be8fcf4cbb99a5000e8f0
Harshit M <79850332+djharshit@users.noreply.github.com>
2024-05-27 12:22:58 +0530

    Show the current version

--- version.py
+++ version.py
@@ -1,18 +1,20 @@
+from sys import version
 import requests
 from bs4 import BeautifulSoup
 
 try:
-    res = requests.get("https://www.python.org/downloads/")
-    soup = BeautifulSoup(res.text, "html.parser")
-    items = soup.find_all(name="ol", class_="list-row-container menu")[1]
-    version = items.find_all(name="span", class_="release-number")
-    date = items.find_all(name="span", class_="release-date")
+    print("Current Python version:", version)
+    res = requests.get('https://www.python.org/downloads/')
+    soup = BeautifulSoup(res.text, 'html.parser')
+    items = soup.find_all(name='ol', class_='list-row-container menu')[1]
+    version = items.find_all(name='span', class_='release-number')
+    date = items.find_all(name='span', class_='release-date')
 
     for i in range(10):
-        print(version[i].text, date[i].text)
+        print(version[i].text, '-', date[i].text)
 
 except:
-    print("Not connected")
+    print('Not connected to internet')
 
 finally:
     input()
